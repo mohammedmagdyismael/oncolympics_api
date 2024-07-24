c8658b030d624611e681dc65d397133827c64a67
mohamed magdy ismael <mohamed.magdy@vezeeta.com>
2024-07-24 10:38:15 +0300

    Fix port number

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 // Middleware to parse JSON bodies
 app.use(express.json());
+app.get("/", (req, res) => res.send("Oncolymbics"));
 
 /** Routes */
 // Health Check
@@ -13,6 +14,9 @@ app.use('/api', healthCheck);
 app.use('/api/users', userRoutes);
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on Port: ${PORT}`);
 });
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
