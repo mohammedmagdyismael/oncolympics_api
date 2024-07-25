e9e455b188c813d48077d5100c249ffb0e710698
mohamed magdy ismael <mohamed.magdy@vezeeta.com>
2024-07-25 23:36:50 +0300

    Match

---
+++ Controllers/MatchController.js
@@ -0,0 +1,14 @@
+// Admin
+
+/** Get Current Match by Date First match status 0 or 1 */
+
+/** Set Match Status by match_id, on set finish && First_round: Calculate Groups table score  */
+
+/** Set Next Question ID and Calculate Match scores from Question_Scores */
+
+// Team
+
+// Get Current Macth by user token
+/** Get Current Match by team user token match status 0 or 1 */
+
+/** Set Question Score by match_id and question_id */
