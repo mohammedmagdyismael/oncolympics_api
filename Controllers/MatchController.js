ea5adfc7062e1a73a64f33dba6bd545ca2c717c3
mohamed magdy ismael <mohamed.magdy@vezeeta.com>
2024-08-02 23:22:23 +0300

    Moderator Match

--- Controllers/MatchController.js
+++ Controllers/MatchController.js
@@ -2,49 +2,49 @@
 
 const db = require('../db'); // Assuming you have a database module for handling DB operations
 
-exports.getNextMatch = async (req, res) => {
+exports.getNextMatchModerator = async (req, res) => {
   const token = req.headers.token;
+
 
   try {
     // Query to get the userId from the Users table using the token
     const [user] = await db.query(`SELECT id, role FROM Users WHERE token ="${token}"`);
 
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    let query = '';
-
-    if (user[0].role === 'Team') {
-        query = `SELECT * FROM Matches 
-       WHERE (team1_id IN (
-               SELECT userId 
-               FROM Teams 
-               WHERE userId = ?) 
-           OR team2_id IN (
-               SELECT userId 
-               FROM Teams 
-               WHERE userId = ?))
-       AND match_status <> 2
-       ORDER BY date_time ASC
-       LIMIT 1`;
-    } else {
-        query = `
-            SELECT * FROM Matches 
-            where match_status <> 2
-            ORDER BY date_time ASC
-            LIMIT 1
-        `
+    if (!user || !(user && user.length > 0 && user[0].role === 'Admin')) {
+      return res.status(404).json({ message: 'User not found or not authorized' });
     }
 
     const userId = user[0].id;
 
 
+    const query = `
+      SELECT 
+        Matches.id,
+          team1_id,
+          team_1.name AS team1_name,
+          team_1.logo AS team1_logo,
+          score_team1,
+          team2_id,
+          team_2.name AS team2_name,
+          team_2.logo AS team2_logo,
+          score_team2,
+          date_time,
+          match_type,
+          question_file,
+          match_status
+      FROM 
+          Matches
+      JOIN 
+          Teams team_1 ON Matches.team1_id = team_1.id
+      JOIN 
+          Teams team_2 ON Matches.team2_id = team_2.id
+      where Matches.id in (
+              select currentMatchId from CurrentMatch where id = 0 AND matchAdmin = ${userId}
+      );
+    `;
+
     // Query to get the next match for the user's team
-    const [match] = await db.query(
-        query,
-      [userId, userId]
-    );
+    const [match] = await db.query(query);
 
     if (!match) {
       return res.status(404).json({ message: 'No upcoming match found' });
@@ -57,68 +57,266 @@ exports.getNextMatch = async (req, res) => {
   }
 };
 
-
-exports.updateMatchStatus = async (req, res) => {
-    const { newStatus } = req.body;
+exports.startMatch = async (req, res) => {
     const token = req.headers.token;
-  
-    if (![1, 2].includes(newStatus)) {
-      return res.status(400).json({ error: 'Invalid new status' });
-    }
   
     try {
       // Verify user is Admin
-      const [user] = await db.query('SELECT role FROM Users WHERE token = ?', [token]);
+      const [user] = await db.query('SELECT * FROM Users WHERE token = ?', [token]);
       if (user.length === 0 || user[0].role !== 'Admin') {
         return res.status(403).json({ error: 'User is not authorized to perform this action' });
       }
-  
-      // Get the first match with match_status 0 or 1
-      const [matches] = await db.query(
-        'SELECT id, match_status FROM Matches WHERE match_status IN (0, 1) ORDER BY date_time LIMIT 1'
-      );
-  
-      if (matches.length === 0) {
-        return res.status(404).json({ error: 'No match found with status 0 or 1' });
-      }
-  
-      const match = matches[0];
-      if ((match.match_status === 0 && newStatus === 1) || (match.match_status === 1 && newStatus === 2)) {
-        // Update match status
-        await db.query(`UPDATE Matches SET match_status=${newStatus} WHERE id=${match.id}`);
-        res.status(200).json({ message: `Match status updated to ${newStatus}` });
-      } else {
+
+      const userId = user[0].id;
+      try {
+        // Get match record
+        const matchRecord = `
+          SELECT 
+            *
+          FROM 
+              Matches
+          where Matches.id in (
+                  select currentMatchId from CurrentMatch where id = 0 AND matchAdmin = ${userId}
+          );
+        `;
+        const [match] = await db.query(matchRecord);
+        const matchId = match[0].id;
+
+
+        const questionId = 0;
+        const team1_id = match[0].team1_id;
+        const team2_id = match[0].team2_id;
+        const score_team1 = match[0].score_team1;
+        const score_team2 = match[0].score_team2;
+
+        // Change Match Status to 1
+        const startmatchquery = `
+          UPDATE Matches
+          SET match_status = 1
+          WHERE id = ${matchId};
+        `;
+        await db.query(startmatchquery);
+
+        // Check Existantance of question record in MacthScores Table
+        const firstquestionExistQuery = `
+          select * from MatchScore where matchId = ${matchId} AND questionId = 0;
+        `;
+        const [checkFirstquestionExistExist] = await db.query(firstquestionExistQuery);
+
+        if (!(checkFirstquestionExistExist && checkFirstquestionExistExist.length)) {
+
+          const insertquestionrecordquery = `
+            INSERT INTO MatchScore (matchId, questionId, team1_id, team2_id, score_team1, score_team2)
+            VALUES (${matchId}, ${questionId}, ${team1_id}, ${team2_id}, ${score_team1}, ${score_team2});
+          `;
+
+          await db.query(insertquestionrecordquery);
+          res.status(200).json({ message: `Match ${matchId} started` });
+        } else {
+          res.status(200).json({ message: `Question record Exists` });
+        }
+
+      } catch (err) {
         res.status(400).json({ error: 'Invalid status transition' });
       }
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
-  };
+};
 
-exports.incrementCurrentQuestion = async (req, res) => {
-    const token = req.headers.token;
-  
+const aggregateScores = async (matchId) => {
+  // get match scores
+  const matchscorequery = `
+    SELECT 
+      *
+    FROM MatchScore
+    where MatchScore.matchId = ${matchId}
+  `;
+  const [matchscores] = await db.query(matchscorequery);
+  let team1_totalScore = 0;
+  let team2_totalScore = 0; 
+
+  matchscores.forEach(mq => {
+    team1_totalScore = team1_totalScore + mq.score_team1;
+    team2_totalScore = team2_totalScore + mq.score_team2;
+  });
+
+  const aggregationValuesquery = `
+    UPDATE Matches
+    SET score_team1 = ${team1_totalScore}, score_team1 = ${team2_totalScore} 
+    WHERE id = ${matchId};
+  `;
+    
+  await db.query(aggregationValuesquery);
+}
+
+exports.endMatch = async (req, res) => {
+  const token = req.headers.token;
+
+  try {
+    // Verify user is Admin
+    const [user] = await db.query('SELECT * FROM Users WHERE token = ?', [token]);
+    if (user.length === 0 || user[0].role !== 'Admin') {
+      return res.status(403).json({ error: 'User is not authorized to perform this action' });
+    }
+    const userId = user[0].id;
+
     try {
-      // Verify user is Admin
-      const [user] = await db.query('SELECT role FROM Users WHERE token = ?', [token]);
-      if (user.length === 0 || user[0].role !== 'Admin') {
-        return res.status(403).json({ error: 'User is not authorized to perform this action' });
+      // Get match record
+      const matchRecord = `
+        SELECT 
+          *
+        FROM 
+            Matches
+        where Matches.id in (
+                select currentMatchId from CurrentMatch where id = 0 AND matchAdmin = ${userId}
+        );
+      `;
+      const [match] = await db.query(matchRecord);
+      const matchId = match[0].id;
+
+      const query = `
+        UPDATE Matches
+        SET match_status = 2
+        WHERE id = ${matchId};
+      `;
+    
+      await db.query(query);
+
+      try {
+        await aggregateScores(matchId);
+      } catch (e) {
+        console.log(e);
       }
-  
-      // Increment current_question for the first match with status 0
-      const [matches] = await db.query('SELECT id FROM Matches WHERE match_status = 1 LIMIT 1');
       
-      if (matches.length === 0) {
-        return res.status(404).json({ error: 'No match found with status 0' });
-      }
-  
-      const matchId = matches[0].id;
+      res.status(200).json({ message: `Match ${matchId} ended` });
+    } catch (err) {
+      res.status(400).json({ error: 'Invalid status transition' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+exports.nextquestion = async (req, res) => {
+    const token = req.headers.token;
+    try {
+        // Verify user is Admin
+        const [user] = await db.query('SELECT * FROM Users WHERE token = ?', [token]);
+        if (user.length === 0 || user[0].role !== 'Admin') {
+          return res.status(403).json({ error: 'User is not authorized to perform this action' });
+        }
+
+        const userId = user[0].id;
+      
+        // Get match record
+        const matchRecord = `
+          SELECT 
+            *
+          FROM 
+              Matches
+          where Matches.id in (
+                  select currentMatchId from CurrentMatch where id = 0 AND matchAdmin = ${userId}
+          );
+        `;
+      const [match] = await db.query(matchRecord);
+      const matchId = match[0].id;
+      const nextQuestionId = match[0].current_question + 1;
+      const team1_id = match[0].team1_id;
+      const team2_id = match[0].team2_id;
+      const score_team1 = match[0].score_team1;
+      const score_team2 = match[0].score_team2;
+      // Update Question Index
       await db.query('UPDATE Matches SET current_question = current_question + 1 WHERE id = ?', [matchId]);
-  
-      res.status(200).json({ message: 'Current question index incremented successfully' });
+      
+      // Create Question Record
+      const questionExistQuery = `
+          select * from MatchScore where matchId = ${matchId} AND questionId = ${nextQuestionId};
+        `;
+      const [checkFirstquestionExistExist] = await db.query(questionExistQuery);
+      if (!(checkFirstquestionExistExist && checkFirstquestionExistExist.length)) {
+
+        const insertquestionrecordquery = `
+          INSERT INTO MatchScore (matchId, questionId, team1_id, team2_id, score_team1, score_team2)
+          VALUES (${matchId}, ${nextQuestionId}, ${team1_id}, ${team2_id}, ${score_team1}, ${score_team2});
+        `;
+
+        await db.query(insertquestionrecordquery);
+        
+        try {
+          await aggregateScores(matchId);
+        } catch (e) {
+          console.log(e);
+        }
+
+        res.status(200).json({ message: 'Current question index incremented successfully' });
+      } else {
+        res.status(200).json({ message: `Question record Exists` });
+      }
+
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
-  };
+};
+
+exports.matchScores = async (req, res) => {
+  const token = req.headers.token;
+
+  try {
+    // Verify user is Admin
+    const [user] = await db.query('SELECT * FROM Users WHERE token = ?', [token]);
+    if (user.length === 0 || user[0].role !== 'Admin') {
+      return res.status(403).json({ error: 'User is not authorized to perform this action' });
+    }
+    const userId = user[0].id;
+
+    try {
+      // Get match scores
+      const matchscoresRecord = `
+        SELECT 
+          MatchScore.id,
+            MatchScore.questionId,
+            MatchScore.matchId,
+          team_1.name AS team1_name,
+          team_1.logo AS team1_logo,
+            MatchScore.score_team1,
+            team_2.name AS team2_name,
+            team_2.logo AS team2_logo,
+            MatchScore.score_team2
+        FROM MatchScore
+        JOIN 
+          Teams team_1 ON MatchScore.team1_id = team_1.id
+        JOIN 
+          Teams team_2 ON MatchScore.team2_id = team_2.id
+        where MatchScore.matchId in (
+                select currentMatchId from CurrentMatch where id = 0 AND matchAdmin = ${userId}
+        );
+      `;
+      const [match] = await db.query(matchscoresRecord);
+      
+
+      if (!match) {
+        return res.status(404).json({ message: 'No upcoming match found' });
+      }
+
+      let team1_totalScore = 0;
+      let team2_totalScore = 0; 
+
+      match.forEach(mq => {
+        team1_totalScore = team1_totalScore + mq.score_team1;
+        team2_totalScore = team2_totalScore + mq.score_team2;
+      })
+
+      res.json({ status: 200, data: { team1_totalScore, team2_totalScore, scores: match,  } });
+      
+    } catch (err) {
+      res.status(400).json({ error: 'Invalid status transition' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
--- Routes/MatchRoutes.js
+++ Routes/MatchRoutes.js
@@ -3,9 +3,17 @@ const router = express.Router();
 const matchController = require('../Controllers/MatchController');
 
 // Define the route to get groups with teams ordered by Pts
-router.get('/next-match', matchController.getNextMatch);
-router.post('/update-match-status', matchController.updateMatchStatus);
-router.get('/increment-current-question', matchController.incrementCurrentQuestion);
+ // Admin / Team (Get next match if there's no matches or get current match)
 
+// Admin
+router.get('/moderatormatch', matchController.getNextMatchModerator);
+router.get('/startmatch', matchController.startMatch)
+router.get('/nextquestion', matchController.nextquestion)
+router.get('/endmatch', matchController.endMatch)
+router.get('/matchscores', matchController.matchScores)
+
+// Team Player
+router.get('/playermatch');
+router.get('/answer');
 
 module.exports = router;
