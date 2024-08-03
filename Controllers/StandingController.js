3896c3be2fbb5a83040c88c4d01bb83942f86f0a
mohamed magdy ismael <mohamed.magdy@vezeeta.com>
2024-08-03 20:52:53 +0300

    Add Get match details

--- Controllers/StandingController.js
+++ Controllers/StandingController.js
@@ -6,8 +6,10 @@ exports.getAllMatches = async (req, res) => {
         SELECT 
           t1.name AS team1_name, 
           t1.id AS team1_id, 
+          t1.logo AS team1_logo, 
           t2.name AS team2_name, 
           t2.id AS team2_id,
+          t2.logo AS team2_logo,
           m.score_team1, 
           m.score_team2,
           m.match_type,
@@ -26,3 +28,38 @@ exports.getAllMatches = async (req, res) => {
       res.status(500).json({ error: error.message });
     }
   };
+
+  exports.matchdetails = async (req, res) => {
+
+    const { matchid } = req.query;
+
+    const matchscoresRecord = `
+        SELECT 
+          MatchScore.id,
+            MatchScore.questionId,
+            MatchScore.matchId,
+          team_1.name AS team1_name,
+          team_1.logo AS team1_logo,
+            MatchScore.score_team1,
+            team_2.name AS team2_name,
+            team_2.logo AS team2_logo,
+            MatchScore.score_team2
+        FROM MatchScore
+        JOIN 
+          Teams team_1 ON MatchScore.team1_id = team_1.id
+        JOIN 
+          Teams team_2 ON MatchScore.team2_id = team_2.id
+        where MatchScore.matchId = ${matchid};
+    `;
+
+    try {
+      const [results] = await db.query(matchscoresRecord);
+    res.json({
+        status: 200,
+        data: results
+    });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+
--- Routes/StandingRoutes.js
+++ Routes/StandingRoutes.js
@@ -4,5 +4,6 @@ const standingsController = require('../Controllers/StandingController');
 
 // Route to get all matches
 router.get('/all-matches', standingsController.getAllMatches);
+router.get('/matchdetails', standingsController.matchdetails);
 
 module.exports = router;
