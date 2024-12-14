87dfe1915722e9db126ebf05bfe6d9bb3cd14ed1
mohammedmagdyismael <mohamed.magdy@vezeeta.com>
2024-12-14 22:07:21 +0200

    Add Knockouts

--- src/Controllers/KnockoutsController.js
+++ src/Controllers/KnockoutsController.js
@@ -2,6 +2,50 @@ const db = require('../db');
 const prisma = require('../db_prisma');
 const Constants = require('../utils/Constants');
 
+exports.getAllMatches = async (req, res) => {
+  try {
+    const response = {
+      [`${Constants.STAGES.Second_round}`]: [],
+      [`${Constants.STAGES.Third_round}`]: [],
+      [`${Constants.STAGES.Final_round}`]: [],
+    };
+
+    for (const [key, value] of Object.entries(Constants.STAGES)) {
+      const [results] = await db.query(`
+        SELECT 
+          m.id as match_id,
+          t1.name AS team1_name, 
+          t1.abbrev AS team1_abbrev, 
+          t1.id AS team1_id, 
+          t1.logo AS team1_logo, 
+          t2.name AS team2_name, 
+          t2.abbrev AS team2_abbrev, 
+          t2.id AS team2_id,
+          t2.logo AS team2_logo,
+          m.score_team1, 
+          m.score_team2,
+          m.match_type,
+          m.date_time,
+          m.match_status
+        FROM Matches m
+        JOIN Teams t1 ON m.team1_id = t1.id
+        JOIN Teams t2 ON m.team2_id = t2.id
+        WHERE m.match_type = '${value}'
+        ORDER BY m.date_time
+      `);
+      response[key] = results;
+    }
+
+    res.json({
+      status: 200,
+      data: response,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
 exports.getAllMatchesA = async (req, res) => {
     try {
       const [results] = await db.query(`
--- src/Routes/KnockoutRoutes.js
+++ src/Routes/KnockoutRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const knockoutsController = require('../Controllers/KnockoutsController');
 
 // Route to get all matches
+router.get('/', knockoutsController.getAllMatches);
 router.get('/knockoutsA', knockoutsController.getAllMatchesA); // 8 Teams
 router.get('/knockoutsB', knockoutsController.getAllMatchesB); // 4 Teams
 router.get('/knockoutsfinal', knockoutsController.getAllMatchesFinal); // 2 Teams
